ec51663cc9803211e8a4334a322934db639d90a9
zainulabidins22 <163645810+zainulabidins22@users.noreply.github.com>
2024-03-27 11:40:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var express = require("express")
-var app = express()
-var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+// ->> API route too add two numbers
+app.get("/add/:num1/:num2", (req, res) => {
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).send("Invalid numbers provided");
+  }
+  const result = num1 + num2;
+  res.send(`The sum of ${num1} and ${num2} is ${result}`);
+});
+
+app.listen(port, () => {
+  console.log("App listening on port: " + port);
+});
+
+// ->> By this we can use this route in local server
+// http://localhost:3000/add/num1/num2
+
